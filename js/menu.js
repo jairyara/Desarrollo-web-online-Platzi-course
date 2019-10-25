db6f6a8f4022d7399f02ec6fa2f7a0ab2e0582b2
Jair Yara <jyara@oneago.com>
2019-10-25 12:47:07 -0500

    Menu dynamic

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,21 @@
 const navSlide = () => {
     const burguer = document.querySelector('.burguer');
+    const nav = document.querySelector('.nav-links');
 
     //Event click burguer
     burguer.addEventListener('click', () => {
+
+        //Menu transition action
+        nav.classList.toggle('nav-active');
 
         //Burguer animation
         burguer.classList.toggle('toggle');
     });
 
+   
 }
+
+
+
 
 navSlide();
